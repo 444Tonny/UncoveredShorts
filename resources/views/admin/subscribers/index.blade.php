b22fbd6cbf113ccb30c17a9ab167ca82c20e5aa9
444tonny <tonny.freelancing@gmail.com>
2024-10-27 16:17:16 -0400

    copy-selection

--- resources/views/admin/subscribers/index.blade.php
+++ resources/views/admin/subscribers/index.blade.php
@@ -32,6 +32,8 @@
             <div class="admin-content">
                 <div class="row">
                     <a href="{{ route('subscribers.writeEmail') }}" class="primary-btn my-btn create"><b>+</b> Schedule a new email</a>
+                    
+                    <button style="width:150px;margin-left:10px;" onclick="copySelection()">Copy Selection</button>
                 </div>
     
                 @if($subscribers->isEmpty())
@@ -40,6 +42,7 @@
                     <table class="sub-table table">
                         <thead>
                             <tr>
+                                <th></th>
                                 <th>Email</th>
                                 <th>Subscribed at</th>
                                 <th style="text-align:center;">
@@ -57,10 +60,9 @@
                         <tbody>
                             @foreach($subscribers as $subscriber)
                                 <tr>
-                                    <td style='width:60%;'>
-                                        <button class='sub-copy'>
-                                            <?xml version="1.0" encoding="utf-8"?><svg fill="#494949" width='16' height='16' class='copy-icon' data-email="{{ $subscriber->email }}" id="copy-icon-{{ $subscriber->id }}" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 115.77 122.88" style="enable-background:new 0 0 115.77 122.88" xml:space="preserve"><style type="text/css">.st0{fill-rule:evenodd;clip-rule:evenodd;}</style><g><path class="st0" d="M89.62,13.96v7.73h12.19h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02v0.02 v73.27v0.01h-0.02c-0.01,3.84-1.57,7.33-4.1,9.86c-2.51,2.5-5.98,4.06-9.82,4.07v0.02h-0.02h-61.7H40.1v-0.02 c-3.84-0.01-7.34-1.57-9.86-4.1c-2.5-2.51-4.06-5.98-4.07-9.82h-0.02v-0.02V92.51H13.96h-0.01v-0.02c-3.84-0.01-7.34-1.57-9.86-4.1 c-2.5-2.51-4.06-5.98-4.07-9.82H0v-0.02V13.96v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07V0h0.02h61.7 h0.01v0.02c3.85,0.01,7.34,1.57,9.86,4.1c2.5,2.51,4.06,5.98,4.07,9.82h0.02V13.96L89.62,13.96z M79.04,21.69v-7.73v-0.02h0.02 c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v64.59v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h12.19V35.65 v-0.01h0.02c0.01-3.85,1.58-7.34,4.1-9.86c2.51-2.5,5.98-4.06,9.82-4.07v-0.02h0.02H79.04L79.04,21.69z M105.18,108.92V35.65v-0.02 h0.02c0-0.91-0.39-1.75-1.01-2.37c-0.61-0.61-1.46-1-2.37-1v0.02h-0.01h-61.7h-0.02v-0.02c-0.91,0-1.75,0.39-2.37,1.01 c-0.61,0.61-1,1.46-1,2.37h0.02v0.01v73.27v0.02h-0.02c0,0.91,0.39,1.75,1.01,2.37c0.61,0.61,1.46,1,2.37,1v-0.02h0.01h61.7h0.02 v0.02c0.91,0,1.75-0.39,2.37-1.01c0.61-0.61,1-1.46,1-2.37h-0.02V108.92L105.18,108.92z"/></g></svg>
-                                        </button> {{ $subscriber->email }}
+                                    <td><input name='sub-selection' type="checkbox" value='{{ $subscriber->email }}' id="sub-checkbox-{{ $subscriber->id }}"></td>
+                                    <td style='width:50%;'>
+                                        {{ $subscriber->email }}
                                     </td>
                                     <td>{!! date('F j, Y', strtotime($subscriber->created_at)) !!}</td>
                                     <td style="text-align:center;">  
@@ -85,7 +87,24 @@
         
         @endsection
 
+        <div id="selectedEmails"></div>
+
         <script>
+            function copySelection() {
+                // Sélectionne toutes les cases cochées
+                const checkboxes = document.querySelectorAll('input[name="sub-selection"]:checked');
+                
+                // Récupère les valeurs (e-mails) et les met au format souhaité
+                const emails = Array.from(checkboxes).map(cb => cb.value).join(', ');
+                
+                // Copie les e-mails dans le presse-papiers
+                navigator.clipboard.writeText(emails).then(() => {
+                    alert("Selected emails have been copied!");
+                }).catch(err => {
+                    console.error('Échec de la copie : ', err);
+                });
+            }
+
             document.addEventListener('DOMContentLoaded', function () {
                 const copyIcons = document.querySelectorAll('.copy-icon');
         
